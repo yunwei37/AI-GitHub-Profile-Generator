b08593f348e3ef6e973808d0e489a6ecfee8eb27
云微 <1067852565@qq.com>
2023-07-03 13:52:48 +0000

    fix stat

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,20 @@ import {
 } from "eventsource-parser";
 import ReactMarkdown from 'react-markdown';
 
+const promptTemplate = `Generate a beatuiful github profile README with GPT and AI, base on the input prompt. 
+the user stats are: 
+"""
+{{userStats}}}
+"""
+Analyzing the user's github profile, and generating a beautiful README for the user.
+You can use the following stats, replace xxxxx with github username:
+"""
+![Github Stats](https://github-readme-stats.vercel.app/api?username=xxxxx
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=xxxxx
+"""
+Output the generated README in markdown format.
+`;
+
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
   const [bio, setBio] = useState("");
@@ -29,133 +43,6 @@ const Home: NextPage = () => {
       bioRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
-
-  const promptTemplate = `Generate a beatuiful github profile README with GPT and AI, base on the input prompt. 
-  the user stats are: 
-  """
-  {{userStats}}}
-  """
-  A beatuiful github profile README  example is:
-  """
-  {{example}}}
-  """
-  Output the generated README in markdown format.
-  `;
-
-  const example = `
-  <h1 align="center">
-    <a href="https://git.io/typing-svg">
-      <img src="https://readme-typing-svg.herokuapp.com/?lines=Hello,+There!+👋;This+is+Osman+DURDAĞ....;Nice+to+meet+you!&center=true&size=30">
-    </a>
-  </h1>
-  
-  <h5 align="center">
-    <code><a href="https://www.linkedin.com/in/osmandurdag/" title="LinkedIn Profile"><img width="22" src="images/linkedin.svg"> LinkedIn</a></code>
-    <code><a href="https://www.hackerrank.com/zumrudu_anka" title="HackerRank Profile"><img width="22" src="images/hackerrank.png"> HackerRank</a></code>
-    <code><a href="https://stackoverflow.com/users/12578260/osman-durdag" title="Stack Overflow Profile"><img width="22" src="images/stackoverflow.svg"> Stack Overflow</a></code>
-    <code><a href="https://www.instagram.com/osman__durdag/" title="Instagram Profile"><img width="22" src="images/instagram.svg"> Instagram</a></code>
-  </h5>
-  <br>
-  <p align="center">
-    Hi, I'm Osman DURDAĞ, Research Assistant & Computer Engineer & Software Developer from Turkey
-    <br>
-    <br>
-    🔬 I'm currently studying for a master's degree in Atatürk University Computer Engineering Department
-    <br>
-    🎓 I graduated from Black Sea Technical University Computer Engineering Department
-    <br>
-    🎓 I graduated from Erzurum İbrahim Hakkı Science High School
-    <br>
-    💻 I love writing code and learn anythings about it
-    <br>
-    📚 I’m currently learning how to build E-Commerce Website with Django
-    <br>
-    💬 Ask me anything about from <a href="https://github.com/zumrudu-anka/zumrudu-anka/issues" title="Issues">Here</a>
-    <br>
-    📫 How to reach me: <a href="mailto: osmandurdag@hotmail.com">osmandurdag@hotmail.com</a>
-  </p>
-  
-  <hr>
-  <h2 align="center">🔥 Languages & Frameworks & Tools & Abilities 🔥</h2>
-  <br>
-  <p align="center">
-    <code><img title="C" height="25" src="images/c.svg"></code>
-    <code><img title="C++" height="25" src="images/cpp.svg"></code>
-    <code><img title="C#" height="25" src="images/cSharp.svg"></code>
-    <code><img title="Python" height="25" src="images/python-original.svg"></code>
-    <code><img title="Django" height="25" src="images/django.png"></code>
-    <code><img title="Javascript" height="25" src="images/javascript.svg"></code>
-    <code><img title="Problem Solving" height="25" src="images/problemSolving.png"></code>
-    <code><img title="HTML5" height="25" src="images/html5.svg"></code>
-    <code><img title="CSS" height="25" src="images/css.svg"></code>
-    <code><img title="SASS" height="25" src="images/sass.svg"></code>
-    <code><img title="Gulp" height="25" src="images/gulp.svg"></code>
-    <code><img title="React" height="25" src="images/react-original.svg"></code>
-    <code><img title="Redux" height="25" src="images/redux.svg"></code>
-    <code><img title="AngularJS" height="25" src="images/angularjs.png"></code>
-    <code><img title="Git" height="25" src="images/git-original.svg"></code>
-    <code><img title=".NetCore" height="25" src="images/dotnetcore.svg"></code>
-    <code><img title="PostgreSQL" height="25" src="images/postgresql.svg"></code>
-    <code><img title="Visual Studio Code" height="25" src="images/vscode.png"></code>
-    <code><img title="Microsoft Visual Studio" height="25" src="images/visualstudio.png"></code>
-    <code><img title="JQuery" height="25" src="images/jquery-original.svg"></code>
-    <code><img title="Java" height="25" src="images/java-original.svg"></code>
-    <code><img title="JSON" height="25" src="images/json.svg"></code>
-    <code><img title="Unity" height="25" src="images/unity3d.svg"></code>
-    <code><img title="Android" height="25" src="images/android.svg"></code>
-    <code><img title="GitHub" height="25" src="images/github.svg"></code>
-    <code><img title="MySQL" height="25" src="images/mysql.svg"></code>
-    <code><img title="npm" height="25" src="images/npm.svg"></code>
-    <code><img title="PHP" height="25" src="images/php.svg"></code>
-    <code><img title="Flask" height="25" src="images/flask.png"></code>
-  </p>
-  <hr>
-  
-  <h2 align="center">⚡ Stats ⚡</h2>
-  <br>
-  <p align=center>
-    <div align=center>
-      <a href="https://github.com/denvercoder1/github-readme-streak-stats" title="Go to Source">
-        <img align="left" width=390 src="https://github-readme-streak-stats.herokuapp.com/?user=zumrudu-anka&theme=react&border=61dafb&hide_border=true" alt="zumrudu-anka" />
-      </a>
-      <a href="https://github.com/anuraghazra/github-readme-stats" title="Go to Source">
-        <img align="right" width=390 src="https://github-readme-stats.vercel.app/api?username=zumrudu-anka&show_icons=true&theme=react&border_color=61dafb&hide_border=true" />
-      </a>
-    </div>
-    <br><br><br><br><br><br><br><br><br>
-    <div align=center>
-      <a href="https://github.com/anuraghazra/github-readme-stats">
-        <img width=325 align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=zumrudu-anka&hide=c%23,powershell,Mathematica,Ruby,Objective-C,Objective-C%2b%2b,Cuda&title_color=61dafb&text_color=ffffff&icon_color=61dafb&bg_color=20232a&langs_count=8&layout=compact&border_color=61dafb&hide_border=true" />
-      </a>
-    </div>
-    <br>
-  
-    <img src="https://github-readme-activity-graph.vercel.app/graph?username=zumrudu-anka&theme=react-dark&bg_color=20232a&hide_border=true" width="100%"/>
-  </p>
-  
-  <hr>
-  
-  <h2 align="center">👨‍💻 Repositories 👨‍💻</h2>
-  <br>
-  <div width="100%" align="center">
-    <a align="left" href="https://github.com/zumrudu-anka/Algorithms" title="Algorithms"><img align="left" height="115" src="https://github-readme-stats.vercel.app/api/pin/?username=zumrudu-anka&repo=Algorithms&theme=react&border_color=61dafb&border_radius=10"></a><a align="right" href="https://github.com/zumrudu-anka/DataStructures" title="Data Structures"><img align="right" height="115" src="https://github-readme-stats.vercel.app/api/pin/?username=zumrudu-anka&repo=DataStructures&theme=react&border_color=61dafb&border_radius=10"></a>
-  </div>
-  <br/><br/><br/><br/><br/><br/>
-  <div width="100%" align="center">
-    <a align="left" href="https://github.com/zumrudu-anka/Turkce-Heceleme-CPP" title="Turkce-Heceleme-CPP"><img align="left" height="115" src="https://github-readme-stats.vercel.app/api/pin/?username=zumrudu-anka&repo=Turkce-Heceleme-CPP&theme=react&border_color=61dafb&border_radius=10"></a>
-    <a align="right" href="https://github.com/zumrudu-anka/CopyMoveForgeryDetectionWithDCT" title="Copy&Move Forgery Detection With DCT"><img align="right" height="115" src="https://github-readme-stats.vercel.app/api/pin/?username=zumrudu-anka&repo=CopyMoveForgeryDetectionWithDCT&theme=react&border_color=61dafb&border_radius=10"></a>
-  </div>
-  <br/><br/><br/><br/><br/><br/>
-  <div width="100%" align="center">
-    <a align="left" href="https://github.com/zumrudu-anka/cpp-openmp-needleman-wunsch" title="Needleman Wunsch Algorithm With OpenMP"><img align="left" height="115" src="https://github-readme-stats.vercel.app/api/pin/?username=zumrudu-anka&repo=cpp-openmp-needleman-wunsch&theme=react&border_color=61dafb&border_radius=10"></a>
-    <a align="right" href="https://github.com/zumrudu-anka/javascript-minesweeper" title="Minesweeper"><img align="right" height="115" src="https://github-readme-stats.vercel.app/api/pin/?username=zumrudu-anka&repo=javascript-minesweeper&theme=react&border_color=61dafb&border_radius=10"></a>
-  </div>
-  <br/><br/><br/><br/><br/><br/>
-  
-  <h4 align="center">
-    <a href="https://github.com/zumrudu-anka?tab=repositories" title="Show Repositories">🔎 Show More 🔍</a>
-  </h4>
-  `
 
   // get github user info from api/github/[username]
   async function getUserStats(username: string): Promise<string> {
@@ -325,7 +212,8 @@ const Home: NextPage = () => {
                   }}
                   key={generatedBios}
                 >
-                  <ReactMarkdown>{generatedBios}</ReactMarkdown>
+                  <ReactMarkdown unwrapDisallowed={true}
+                  >{generatedBios}</ReactMarkdown>
                 </div>
               </div>
             </>
