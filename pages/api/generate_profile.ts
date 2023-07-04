043ac37f5db522f7cb3b091debc3e8779ad02305
云微 <1067852565@qq.com>
2023-07-04 09:42:42 +0000

    add style prompt

--- pages/api/generate_profile.ts
+++ pages/api/generate_profile.ts
@@ -9,6 +9,63 @@ export const config = {
   runtime: "edge",
 };
 
+const stylePrompts = [
+  `
+  use a descriptive structure with all the sections (Overview, Demo, Getting Started, etc.)
+  for example:
+  ## Hi there 👋,
+
+  ### I'm Saksham Taneja, I am a community guy who loves tech and see myself as product enthoziast.
+  -------
+    
+  ## 🧐 About
+  
+  - 🏄‍ Community guy who loves being involved in communities and help students grow
+  - 😄 I will usually be a part of hackathons as a #Mentor, #Participants and a #Organiser
+  - 🔭 I am currently an MSFT Learn Ambassador, [Mozillian](https://mozillians.org/en-US/u/tanejasaksham/), IBM ZAmbassador, Ex-Google DSC Lead and been a part of many communities and programs by a big force
+  - 🌱 I love to speak at public events and have been a speaker at many events. I organise Workshops, Webinars etc to help student Communitiess
+  - 👯 And Many More...
+  
+  
+  - 💬 Ask me about anything and everything!
+  - 📫 How to reach me: message me at [Whatsapp](https://wa.me/919829599750)
+  - ⚡ Fun fact: I love meeting new people and eating food!
+  - 💬 Ping Me about #cloud, #RPA, #CommunityOps, #DevRel, #speaking opportunity, #Marketing #CampusOps and anything you like
+  
+  ...adding more visual elements like GIFs, Images, Badges, etc for better user engagement.
+  Use a simple and clean structure, focus on the content and analysis.
+  `,
+  `
+  Use a code like structure, creatively struct descriptions like a code
+  for example:
+  const anmol = {
+    pronouns: "He" | "Him",
+    code: ["Javascript", "Python", "Java", "PHP"],
+    askMeAbout: ["web dev", "tech", "app dev", "photography"],
+    technologies: {
+        backEnd: {
+            js: ["Node", "Fastify", "Express"],
+        },
+        mobileApp: {
+            native: ["Android Development"]
+        },
+        devOps: ["AWS", "Docker🐳", "Route53", "Nginx"],
+        databases: ["mongo", "MySql", "sqlite"],
+        misc: ["Firebase", "Socket.IO", "selenium", "open-cv", "php", "SuiteApp"]
+    },
+    architecture: ["Serverless Architecture", "Progressive web applications", "Single page applications"],
+    currentFocus: "No Focus point at this time",
+    funFact: "There are two ways to write error-free programs; only the third one works"
+  };
+  ...adding more visual elements like GIFs, Images, Badges, etc for better user engagement, 
+  and a few simple welcome messages.  
+    `,
+  `
+    add more images, badges, etc. Use less text and more visual elements, structure the README in a more creative way
+    Use a simple and clean structure, focus on the content and analysis
+    `
+];
+
 const promptTemplateGenerate = `
 Craft a dynamic and visually compelling GitHub profile README,
 based on user-provided insights and additional requirements.
@@ -24,10 +81,11 @@ The README should:
 - Provide a deep analysis and comprehensive self-introduction based on the user's GitHub profile
 
 **Style & Design:** The README should be visually appealing. 
-You can choose one from the following styles to showcase the user's skills and conclusions:
-a. Use a code like structure, creatively struct descriptions like a code
-b. add more images, badges, etc. Use less text and more visual elements, structure the README in a more creative way
-c. Use a simple and clean structure, focus on the content and analysis
+You can use a style like:
+"""
+{{style}}
+"""
+
 
 **Format & Structure:** Use Markdown for the layout with visually appealing elements, such as:
 
@@ -66,9 +124,13 @@ const handler = async (req: Request): Promise<Response> => {
     return new Response("No prompt in the request", { status: 400 });
   }
 
-  const prompt = promptTemplateGenerate.
-  replace("{{user insights}}", insight).
-  replace("{{requirements}}", requirements || "");
+  let prompt = promptTemplateGenerate.
+    replace("{{user insights}}", insight).
+    replace("{{requirements}}", requirements || "");
+  // choose a random style prompt
+  const stylePrompt = stylePrompts[Math.floor(Math.random() * stylePrompts.length)];
+  // add style prompt to the main prompt
+  prompt = prompt.replace("{{style}}", stylePrompt);
   console.log(prompt);
   const payload: OpenAIStreamPayload = {
     model: "gpt-3.5-turbo-16k",
